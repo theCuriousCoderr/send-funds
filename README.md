3dfb3ab64f8434779897ff40f51a4c6067e9d338
tomurashigaraki22 <emmanuelhudson355@gmail.com>
2024-12-15 18:20:16 +0100

    new world order

---
+++ README.md
@@ -0,0 +1,8 @@
+# Send me money
+
+# Follow me on Twitter at [open](https://x.com/honour_can_code)
+# Follow me on TIktok at [open](https://tiktok.com/@codewithhonour)
+ 
+ 
+ 
+ # Fund-Me
---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import Banner from "./components/Banner";
+
+function Home() {
+  return (
+    <div className="bg-[#000] min-h-screen">
+      <Banner />
+    </div>
+  );
+}
+
+export default Home;
